d3a1fb1e5e5ab40ca9d634efad8021c9d4af0ce6
CycloPi <venti3d@gmail.com>
2017-11-20 21:47:23 +0100

    Hub retocao

--- Pinball/Hub/Hub.gd
+++ Pinball/Hub/Hub.gd
@@ -9,10 +9,10 @@ func _ready():
 func _process(delta):
 	
 	contador_bolas = get_node("Bolas")
-	contador_bolas.set_text("Bolas:"+str(GLOBAL.bolas))
+	contador_bolas.set_text("STOCK BALL:"+str(GLOBAL.bolas))
 	
 	contador_bolas_juego = get_node("Bolas en juego")
-	contador_bolas_juego.set_text("Bolas en juego:"+str(GLOBAL.bolas_juego))
+	contador_bolas_juego.set_text("PLAY BALL:"+str(GLOBAL.bolas_juego))
 	
 	contador_puntos= get_node("Puntos")
-	contador_puntos.set_text("Puntos:"+str(GLOBAL.puntos))
+	contador_puntos.set_text("SCORE:"+str(GLOBAL.puntos))
