f6b97301f77e4c16b6c30b74c7fb587fdd015b12
juanantoniogit <juanantonio.tecno@gmail.com>
2017-11-20 21:22:43 +0100

    Añado Dianas
    Se han añadido las dianas aun que no estan totalmente funcionales.

--- Pinball/Base/golpeador_izq.gd
+++ Pinball/Base/golpeador_izq.gd
@@ -10,6 +10,7 @@ func _fixed_process(delta):
 		if rotacion < 1:
 			rotacion += 0.25
 			set_rotation(Vector3(0,rotacion,0))
+		
 	if not Input.is_action_pressed("ui_left"):
 		if rotacion > 0:
 			rotacion -= 0.25
